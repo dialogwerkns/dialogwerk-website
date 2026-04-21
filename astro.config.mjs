c3919dc88fcfe3eb673c3e85f337acbeda3f1509
dialogwerkns <info@dialogwerk-niedersachsen.de>
2026-04-21 16:13:46 +0200

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-
-export default defineConfig({
-    site: 'https://dialogwerk-niedersachsen.de',
-    integrations: [tailwind(), sitemap()],
-});
